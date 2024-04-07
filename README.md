e6e4f32cb63a6d1c932f4148454cb2b629b1793b
Ananya-git24 <166315547+Ananya-git24@users.noreply.github.com>
2024-04-07 21:39:02 +0530

    Add files via upload

--- README.md
+++
@@ -1,1 +0,0 @@
-# wellnet-using-Llama2
---
+++ src/prompt.py
@@ -0,0 +1,10 @@
+prompt_template = """
+Utilize the provided details to respond to the user's inquiry.
+If you lack the answer, simply state that you're unaware, refrain from inventing a response.
+
+Context: {context}
+Queston: {question}
+
+Kindly provide solely the useful response below.
+Useful response:
+"""
